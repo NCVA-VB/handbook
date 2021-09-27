0d08e69b73bafea6a3c6f78a63cd63436c6ebd20
NCVA <87445016+NCVA-VB@users.noreply.github.com>
2021-09-27 16:02:12 -0700

    Refactor handbook tools. Add exportsection, rendermarkdown

--- handbooktools.js
+++ handbooktools.js
@@ -7,21 +7,67 @@ const fsPath = require( 'path' );
 const clip = require( 'copy-paste' );
 // const readline = require( 'readline' );
 
-const { readFileAsJSON } = require( './utils/fileHelpers' );
+const handbookdata = require( './handbookdata' );
+const { readFileAsText } = require( './utils/fileHelpers' );
 const { tableFormatters } = require( './utils/tableFormatters' );
-const { markdown } = require( './utils/markdownTools' );
+const { markdown: md } = require( './utils/markdownTools' );
+const { replaceTokens } = require( './utils/tokenReplacers' );
+
+function getTokens( handbookdata, handbook ) {
+  return { ...handbookdata.commonTokens, ...handbook.tokens };
+}
+
+async function loadSection(  handbookdata, book, section ) {
+
+  console.log( book.name );
+  console.log( section.sectionName );
+
+  if ( !section ) {
+    console.log( `Section not found: ${section.sectionName}` );
+    process.exit( 0 );
+  }
+
+  const path = ( section.path ) ?
+    fsPath.join( __dirname, ...section.path, `${section.fileName}.md` ) :
+    fsPath.join( __dirname, book.name, `${section.fileName}.md` );
+
+  const rawText = await readFileAsText( path );
+
+  if ( !rawText ) {
+    console.log( `FILE NOT FOUND: ${path}` );
+    console.log( section );
+  }
+
+  return rawText;
+
+}
+
+function filterForTableTokens( tokens ) {
+  return tokens.filter( ( k ) => ( k.slice( 0, 6 ) === 'table_'  ) );
+}
 
 
 const commands = {
+  'exportsection': async function exportSection( handbookdata, argv ) {
+
+    const book = handbookdata.handbooks.find( ( b ) => b.name === argv.bookname );
+    const section = book.sections.find( ( s ) => s.fileName === argv.sectionname );
+    const text = await loadSection( handbookdata, book, section );
+    const markdown = await replaceTokens( getTokens( handbookdata, book ), text, true );
+    const html = md.render( markdown );
+    clip.copy( html, () => process.exit( 0 ) );
+
+  },
   'exporttable': async function exportTable( handbookdata, DO_REPLACETOKENS, argv ) {
 
     const book = handbookdata.handbooks.find( ( book ) => book.name === argv.bookname );
-    const table = book.tokens[argv.tablename];
+    const tokens = { ...handbookdata.commonTokens, ...book.tokens };
+    const table = tokens[argv.tablename];
+    const text = await tableFormatters[argv.tablename]( table, tokens );
 
-    const md = await tableFormatters[argv.tablename]( table, { ...handbookdata.commonTokens, ...book.tokens } );
     const html = [
       `<!-- ${argv.tablename}-->`,
-      markdown.render( md ),
+      md.render( text ),
     ];
 
     clip.copy( html.join( '\n' ), () => {
@@ -70,6 +116,12 @@ const commands = {
 
 
   },
+  'rendermarkdown': async function renderMarkdown( handbookdata, argv ) {
+
+    const content = clip.paste();
+    clip.copy( md.render( content ), () => process.exit( 1 ) );
+
+  },
 };
 
 console.log( '' );
@@ -77,22 +129,16 @@ console.log( '' );
 
 const DO_REPLACETOKENS = true;
 
-async function runTools() {
+async function runTools( handbookdata ) {
 
-  const handbookdata = await readFileAsJSON( fsPath.join( __dirname, 'handbookdata.json' ) );
+  const tableNames = new Set( filterForTableTokens( Object.keys( handbookdata.commonTokens ) ) );
 
-  const tableNames = handbookdata.handbooks.reduce( ( names, book ) => {
+  handbookdata.handbooks.forEach( ( book ) => {
 
-    return Object.keys( book.tokens ).reduce( ( names, k ) => {
+    filterForTableTokens( Object.keys( book.tokens ) )
+      .forEach( ( t ) => tableNames.add( t ) );
 
-      if ( k.slice( 0, 6 ) === 'table_' )
-        names.add( k );
-
-      return names;
-
-    }, names );
-
-  }, new Set() );
+  } );
 
   yargs
     .command(
@@ -111,10 +157,31 @@ async function runTools() {
       },
     )
     .command(
+      'exportsection <bookname> <sectionname>',
+      'Export a book section and render to clipboard.',
+      {
+        'bookname': {
+          'alias': 'b',
+        },
+        'sectionname': {
+          'alias': 's',
+        },
+      },      async ( argv ) => {
+        commands.exportsection( handbookdata, argv );
+      },
+    )
+    .command(
       'exporttokens',
       'Export tokens into spreadsheet format and place in the clipboard.',
       async ( argv ) => {
         commands.exporttokens( handbookdata, argv );
+      },
+    )
+    .command(
+      'clipboardtohtml',
+      'Render clipboard contents as HTML (clipboard contents should be in markdown format).',
+      async ( argv ) => {
+        commands.rendermarkdown( handbookdata, argv );
       },
     )
     .choices( 'tablename', [...tableNames] )
@@ -129,4 +196,4 @@ async function runTools() {
 
 }
 
-runTools();
+runTools( handbookdata );
